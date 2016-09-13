934bc6195e9f8c7fb4496dabb8010e060dd36743
AndrewKahora <cahndrew@gmail.com>
2016-09-13 10:57:00 +0300

    show and hide side nav

--- js/app.js
+++ js/app.js
@@ -4,3 +4,7 @@ menuWidth: 300, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
+ // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
