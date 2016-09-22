8e1bce180797c84fdf3a4cbd1b21968fd8b5169f
AndrewKahora <cahndrew@gmail.com>
2016-09-22 11:37:33 +0300

    slider e

--- js/app.js
+++ js/app.js
@@ -8,3 +8,23 @@ menuWidth: 300, // Default is 240
   $('.button-collapse').sideNav('show');
   // Hide sideNav
   $('.button-collapse').sideNav('hide');
+
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+
+});
